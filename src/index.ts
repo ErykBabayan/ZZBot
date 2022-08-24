6bdac1d137c0ecfd30d07557e06c2ebee6d551c8
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-08-24 17:13:43 +0200

    add basic client interaction

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello Discord Bot")
+import config from "./config"
+import { Client, GatewayIntentBits } from "discord.js"
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds]})
+
+client.once('ready', () => {
+	console.log('Beep Boop');
+});
+
+client.login(config.DISCORD_TOKEN);
