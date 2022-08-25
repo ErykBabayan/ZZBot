bd2c78a1315d6f341b1a484e9eb5dc4366a2b95a
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-08-25 15:50:29 +0200

    Add basic test commands

---
+++ src/commands/deploy-commands.ts
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const config = require("./config")
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(config.DISCORD_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import config from "./config"
-import { Client, GatewayIntentBits } from "discord.js"
+const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds]})
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', () => {
-	console.log('Beep Boop');
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction: {
+	[x: string]: any; isChatInputCommand?: any; reply?: any; commandName?: any; //TODO Fix later
+}) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server members count: ${interaction?.guild.id}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`User ${interaction.user.tag}`);
+	}
 });
 
 client.login(config.DISCORD_TOKEN);
