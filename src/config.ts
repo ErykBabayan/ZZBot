4836bb29ae94b157ae170c6fcf9b87a9295b7aaf
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-08-25 15:50:00 +0200

    add clientId and guildId with validation

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,15 @@
 import dotenv from "dotenv"
 
 dotenv.config()
-const{DISCORD_TOKEN} = process.env
+const{DISCORD_TOKEN, CLIENT_ID, GUILD_ID} = process.env
 
-if(!DISCORD_TOKEN){
+if(!DISCORD_TOKEN || !CLIENT_ID || !GUILD_ID){
     throw new Error("Missing environment variables")
 }
 const config = {
-    DISCORD_TOKEN
+    DISCORD_TOKEN,
+    CLIENT_ID,
+    GUILD_ID
 }
 
 export default config
