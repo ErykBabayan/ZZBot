a78e224679d39b46de873df3bbefc24cfaa2146e
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-08-31 19:16:22 +0200

    typefix for totalMemberCounter

--- src/counters/total-member-counter.ts
+++ src/counters/total-member-counter.ts
@@ -1,13 +1,20 @@
-import { Client } from "discord.js"
-import config from "../config"
-const totalMemberCounter = async (client: Client<boolean>) => { 
-    setInterval(()=>{
-        const guild = client.guilds.cache.get(config.GUILD_ID)
-        const memberCount = guild?.memberCount
-        const channel = guild?.channels.cache.get("1012755948709884064")
-        channel?.setName(`Total Members: ${memberCount?.toLocaleString()}`)
-        console.log("updating memberCount: ",memberCount)
-    },5000) // TODO: change time to every 4-6h
+import { Client, Guild } from "discord.js";
+import config from "../config";
 
-}
-export default totalMemberCounter
+const totalMemberCounter = async (client: Client<boolean>) => {
+	const guild = client.guilds.cache.get(config.GUILD_ID);
+	if (guild === undefined) {
+		throw new TypeError("The object must not be undefined, aborting");
+	}
+	setInterval(() => {
+		const channel = guild.channels.cache.get("1012755948709884064");
+		if (channel === undefined) {
+			throw new TypeError("The object must not be undefined, aborting");
+		}
+		const memberCount = guild.memberCount;
+		channel.setName(`Total Members: ${memberCount}`);
+		console.log("updating memberCount: ", memberCount);
+	}, 300000); // TODO: change time to every 4-6
+};
+
+export default totalMemberCounter;
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
-import config from "./config"
-import { Client, GatewayIntentBits }  from 'discord.js';
-import totalMemberCounter from "./counters/total-member-counter"
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+import config from "./config";
+import { Client, GatewayIntentBits } from "discord.js";
+import totalMemberCounter from "./counters/total-member-counter";
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers],
+});
+
+client.on("ready", () => {
+	console.log("ZŻ Bot is online!");
+	totalMemberCounter(client);
+});
 
 client.on('ready', () => {
 	console.log('ZŻ Bot is online!');
 	totalMemberCounter(client)
 });
 
-client.on('interactionCreate', async (interaction: {
-	[x: string]: any; isChatInputCommand?: any; reply?: any; commandName?: any; //TODO Fix later
-}) => {
-	if (!interaction.isChatInputCommand()) return;
+client.on(
+	"interactionCreate",
+	async (interaction:{
+		[x: string]: any;
+		isChatInputCommand?: any;
+		reply?: any;
+		commandName?: any; //TODO Fix later
+	}) => {
+		if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+		const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server ID: ${interaction.guild.id}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Member Count ${interaction.guild.memberCount}`);
+		if (commandName === "ping") {
+			await interaction.reply("pong");
+		} else if (commandName === "server") {
+			await interaction.reply(`Server ID: ${interaction.guild.id}`);
+		} else if (commandName === "user") {
+			await interaction.reply(`Member Count ${interaction.guild.memberCount}`);
+		}
 	}
-});
+);
 
 client.login(config.DISCORD_TOKEN);
