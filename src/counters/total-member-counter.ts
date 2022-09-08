87f114378745fd39f2dcc8876e510dc3b008774b
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-09-08 19:04:40 +0200

    filter bot members from memberCound

--- src/counters/total-member-counter.ts
+++ src/counters/total-member-counter.ts
@@ -1,18 +1,15 @@
-import { Client, Guild } from "discord.js";
+import { Client, Guild, Message } from "discord.js";
 import config from "../config";
 
-const totalMemberCounter = async (client: Client<boolean>) => {
+const totalMemberCounter = async (client: Client<boolean>): Promise<void> => {
 	const guild = client.guilds.cache.get(config.GUILD_ID);
-	if (guild === undefined) {
-		throw new TypeError("The object must not be undefined, aborting");
-	}
+	if (guild === undefined) throw new TypeError("The object must not be undefined, aborting");
+	const channel = guild?.channels.cache.get("1012755948709884064");
 	setInterval(() => {
-		const channel = guild.channels.cache.get("1012755948709884064");
-		if (channel === undefined) {
-			throw new TypeError("The object must not be undefined, aborting");
-		}
-		const memberCount = guild.memberCount;
-		channel.setName(`Total Members: ${memberCount}`);
+		const botMembers = guild?.members.cache.filter((member) => !member.user.bot).size;
+		const memberCount = guild?.memberCount - botMembers;
+
+		channel?.setName(`Total Members: ${memberCount}`);
 		console.log("updating memberCount: ", memberCount);
 	}, 300000); // TODO: change time to every 4-6
 };
