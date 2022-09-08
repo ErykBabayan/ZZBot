0a31dff616bb62e09a4c579b47f3271f9e8eb49e
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-09-08 19:05:26 +0200

    add interval to active member counting and setup dedicated channel

--- src/counters/active-members-counter.ts
+++ src/counters/active-members-counter.ts
@@ -1,11 +1,17 @@
 import { Client, Guild } from "discord.js";
 import config from "../config";
 
-function activeMembersCounter(client: Client<boolean>): void {
+const activeMembersCounter = async(client: Client<boolean>): Promise<void> => {
 	const guild = client.guilds.cache.get(config.GUILD_ID);
-	if (guild === undefined) {
-		throw new TypeError("The object must not be undefined, aborting");
-	}
-	const onlineMembers = guild.members.cache.filter((member) => member.presence?.status === "online").size;
+	const channel = guild?.channels.cache.get("1016392952026775622")
+
+	setInterval(()=>{
+		const onlineMembers = guild?.members.cache.filter((member) => member.presence?.status !== "offline" && !member.user.bot).size;
+		channel?.setName(`Active Members: ${onlineMembers}`)
+		console.log("updating onlineMembers: ", onlineMembers);
+	},180000)
+
+
+
 }
 export default activeMembersCounter;
