6848c6c0556e47cc318a831c95cd3d8440b1b140
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-09-18 21:18:12 +0200

    change slash commands in if else block to command handler

--- src/commands/deploy-commands.ts
+++ src/commands/deploy-commands.ts
@@ -1,12 +1,25 @@
 import { SlashCommandBuilder, Routes } from "discord.js";
 import { REST } from "@discordjs/rest";
 import config from "../config";
+const fs = require("node:fs");
+const path = require("node:path");
 
-const commands = [
-	new SlashCommandBuilder().setName("setup").setDescription("Starts setup for member counters"),
-	new SlashCommandBuilder().setName("server").setDescription("Replies with server info!"),
-	new SlashCommandBuilder().setName("user").setDescription("Replies with user info!"),
-].map((command) => command.toJSON());
+const commands: any = [];
+const commandsPath = path.join(__dirname, "commands");
+
+const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith("js"));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+// const commands = [
+// 	new SlashCommandBuilder().setName("setup").setDescription("Starts setup for member counters"),
+// 	new SlashCommandBuilder().setName("server").setDescription("Replies with server info!"),		SlashCommand if else
+// 	new SlashCommandBuilder().setName("user").setDescription("Replies with user info!"),
+// ].map((command) => command.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import config from "./config";
-import { Client, GatewayIntentBits } from "discord.js";
+const fs = require("node:fs");
+const path:any = require("node:path");
+import { Client, GatewayIntentBits, Collection } from "discord.js";
 import totalMemberCounter from "./counters/total-member-counter";
 import activeMembersCounter from "./counters/active-members-counter";
+
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -12,33 +15,54 @@ const client = new Client({
 	],
 });
 
-client.on("ready", () => {
-	console.log("ZŻ Bot is online!");
-	//totalMemberCounter(client);
-	//activeMembersCounter(client);
-});
+ client.commands = new Collection();
+ const commandPath = path.join(__dirname, "commands"); // joins dir absolute path with commands folder
 
-client.on(
-	"interactionCreate",
-	async (interaction: {
-		[x: string]: any;
-		isChatInputCommand?: any;
-		reply?: any;
-		commandName?: any; //TODO Fix later
-	}) => {
-		if (!interaction.isChatInputCommand()) return;
+ const commandFiles = fs.readdirSync(commandPath).filter((file: string) => file.endsWith(".js")); // readdirSync returns array with filenames in a dir
 
-		const { commandName } = interaction;
+for (const file of commandFiles) {
+	const filePath = path.join(commandFiles, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
 
-		if (commandName === "setup") {
-			interaction.reply("Successfully started setup");
-			await totalMemberCounter(client);
-		} else if (commandName === "server") {
-			await interaction.reply(`Server ID: ${interaction.guild.id}`);
-		} else if (commandName === "user") {
-			await interaction.reply(`Member Count ${interaction.guild.memberCount}`);
-		}
-	}
-);
+// client.on("ready", () => {
+// 	console.log("ZŻ Bot is online!");
+// 	//totalMemberCounter(client);
+// 	//activeMembersCounter(client);
+// 	console.log(commandPath);
+// });
+
+// client.on(
+// 	"interactionCreate",
+// 	async (interaction: {
+// 		[x: string]: any;
+// 		isChatInputCommand?: any;
+// 		reply?: any;
+// 		commandName?: any; //TODO Fix later
+// 	}) => {
+// 		if (!interaction.isChatInputCommand()) return;
+
+// 		// const command = interaction.client.commands.get(interaction.commandName); // fetching command from interaction Collection
+// 		// if (!command) return;
+// 		// try {
+// 		// 	await command.execute(interaction);
+// 		// } catch (error) {
+// 		// 	console.error(error);
+// 		// 	await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+// 		// }
+
+// 		/*  Executing command via if else approach  */
+// 		const { commandName } = interaction;
+// 		if (commandName === "setup") {
+// 			interaction.reply("Successfully started setup");
+// 			await totalMemberCounter(client);
+// 		} else if (commandName === "server") {
+// 			await interaction.reply(`Server ID: ${interaction.guild.id}`);
+// 		} else if (commandName === "user") {
+// 			await interaction.reply(`Member Count ${interaction.guild.memberCount}`);
+// 		}
+// 	}
+// );
 
 client.login(config.DISCORD_TOKEN);
