700248cf1f54291428035f6aae6263a88b9556d2
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-09-19 21:04:54 +0200

    fix to non-working command handler

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,13 +1,12 @@
 import { SlashCommandBuilder, Routes } from "discord.js";
 import { REST } from "@discordjs/rest";
-import config from "../config";
+import config from "./config";
 const fs = require("node:fs");
 const path = require("node:path");
 
-const commands: any = [];
+const commands = [];
 const commandsPath = path.join(__dirname, "commands");
-
-const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith("js"));
+const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -15,14 +14,14 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-// const commands = [
-// 	new SlashCommandBuilder().setName("setup").setDescription("Starts setup for member counters"),
-// 	new SlashCommandBuilder().setName("server").setDescription("Replies with server info!"),		SlashCommand if else
-// 	new SlashCommandBuilder().setName("user").setDescription("Replies with user info!"),
-// ].map((command) => command.toJSON());
-
 const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
 
 rest.put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), { body: commands })
 	.then(() => console.log("Successfully registered application commands."))
 	.catch(console.error);
+
+// const commands = [
+// 	new SlashCommandBuilder().setName("setup").setDescription("Starts setup for member counters"),
+// 	new SlashCommandBuilder().setName("server").setDescription("Replies with server info!"),		SlashCommand if else
+// 	new SlashCommandBuilder().setName("user").setDescription("Replies with user info!"),
+// ].map((command) => command.toJSON());
