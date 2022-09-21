22cc52438d6928665be70118e814e8a534db6694
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-09-21 20:46:04 +0200

    change require to impots and remove unused code

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,8 +1,8 @@
 import { SlashCommandBuilder, Routes } from "discord.js";
 import { REST } from "@discordjs/rest";
 import config from "./config";
-const fs = require("node:fs");
-const path = require("node:path");
+import fs from "node:fs";
+import path from "node:path";
 
 const commands = [];
 const commandsPath = path.join(__dirname, "commands");
@@ -19,9 +19,3 @@ const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
 rest.put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), { body: commands })
 	.then(() => console.log("Successfully registered application commands."))
 	.catch(console.error);
-
-// const commands = [
-// 	new SlashCommandBuilder().setName("setup").setDescription("Starts setup for member counters"),
-// 	new SlashCommandBuilder().setName("server").setDescription("Replies with server info!"),		SlashCommand if else
-// 	new SlashCommandBuilder().setName("user").setDescription("Replies with user info!"),
-// ].map((command) => command.toJSON());
