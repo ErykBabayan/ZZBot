a478ee717daa0ab4ba3b81e4f0b2e97c9eabb5a5
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-09-22 21:53:50 +0200

    add create channel for active member counter

--- src/counters/active-members-counter.ts
+++ src/counters/active-members-counter.ts
@@ -1,17 +1,20 @@
 import { Client, Guild } from "discord.js";
 import config from "../config";
 
-const activeMembersCounter = async(client: Client<boolean>): Promise<void> => {
+const activeMembersCounter = async (client: Client<boolean>): Promise<void> => {
 	const guild = client.guilds.cache.get(config.GUILD_ID);
-	const channel = guild?.channels.cache.get("1016392952026775622")
+	const channel = guild?.channels.cache.get("1016392952026775622");
+	if (guild === undefined) throw new TypeError("The object must not be undefined, aborting");
+	const options: any = {
+		name: `ActiveMembers: ${guild?.members.cache.filter((member) => member.presence?.status !== "offline" && !member.user.bot).size}`,
+		type: 2,
+	};
+	guild.channels.create(options);
 
-	setInterval(()=>{
+	setInterval(() => {
 		const onlineMembers = guild?.members.cache.filter((member) => member.presence?.status !== "offline" && !member.user.bot).size;
-		channel?.setName(`Active Members: ${onlineMembers}`)
+		channel?.setName(`Active Members: ${onlineMembers}`);
 		console.log("updating onlineMembers: ", onlineMembers);
-	},180000)
-
-
-
-}
+	}, 180000);
+};
 export default activeMembersCounter;
