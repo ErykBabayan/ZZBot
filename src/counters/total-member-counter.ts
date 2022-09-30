c278f7b08a353e0b5211dc31258cf382da996b82
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-09-30 23:50:04 +0200

    add logChannel creation and logging total member count

--- src/counters/total-member-counter.ts
+++ src/counters/total-member-counter.ts
@@ -1,18 +1,21 @@
-import { Guild } from "discord.js";
+import { Guild, TextChannel } from "discord.js";
 import getOrCreateChannelId from "../functions/get-or-create-channelId";
 
 const totalMemberCounter = async (guild: Guild): Promise<void> => {
-	const channelName: string = `Total Members: ${guild.memberCount}`;
-	const channelId: Promise<any> = getOrCreateChannelId(guild, channelName);
-
+	const botMembers: number = guild.members.cache.filter((member) => member.user.bot).size;
+	const channelName: string = `Total Members: ${guild.memberCount - botMembers}`;
+	const channelId: Promise<any> = getOrCreateChannelId(guild, channelName, 2);
+	const logChannelId: Promise<any> = getOrCreateChannelId(guild, "logi-zż", 0);
 	setInterval(async () => {
 		const channel = guild.channels.cache.get(await channelId);
+		const logChannel = guild.channels.cache.get(await logChannelId);
 		if (channel === undefined) throw new TypeError("The object must not be undefined, aborting");
-		const botMembers = guild?.members.cache.filter((member) => member.user.bot).size;
-		const memberCount = guild?.memberCount - botMembers;
-		channel?.setName(`Total Members: ${memberCount}`);
-		console.log(`Updating total members in channel ${channel.name} channel id: ${channel.id}, total members: ${memberCount}`);
-	}, 10000); //small interval for testing
+
+		const memberCount: string = (guild.memberCount - botMembers).toLocaleString();
+		channel.setName(`Total Members: ${memberCount}`);
+
+		(logChannel as TextChannel).send(`Updating total members in channel ${channel.name}`);
+	}, 50000);
 };
 
 export default totalMemberCounter;
