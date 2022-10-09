757a39cc399f6968573e45e3b4ceb0a1bc33863c
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-10-09 13:13:53 +0200

    add embed log to active members

--- src/tools/counters/active-members-counter.ts
+++ src/tools/counters/active-members-counter.ts
@@ -11,10 +11,12 @@ const activeMembersCounter = async (guild: Guild, logChannel: GuildBasedChannel 
 	setInterval(async () => {
 		const channel: GuildBasedChannel | undefined = guild.channels.cache.get(await channelId);
 		if (channel === undefined) throw new TypeError("The object must not be undefined nor null, aborting");
-		const onlineMembers: string = guild.members.cache
-			.filter((member) => member.presence?.status !== "offline" && !member.user.bot)
-			.size.toLocaleString();
+		const onlineMembers = (await guild.members.fetch()).filter((member) => !member.user.bot && member.presence?.status !== "offline").size;
 		channel.setName(`Active Members: ${onlineMembers}`);
+		const embedLog = new EmbedBuilder()
+			.setTitle("Updated active members channel")
+			.addFields({ name: "Online Members", value: `${onlineMembers}` });
+		(logChannel as TextChannel).send({ embeds: [embedLog] });
 	}, constants.ACTIVE_MEMBERS_REFRESH_INTERVAL);
 };
 export default activeMembersCounter;
