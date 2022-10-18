ce13ccae4443a5de4a2ea2d81c6f5d54654aa769
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-10-18 22:06:54 +0200

    revert changes due to server permission issies

--- src/tools/counters/active-members-counter.ts
+++ src/tools/counters/active-members-counter.ts
@@ -3,28 +3,23 @@ import getOrCreateChannelId from "../functions/get-or-create-channelId";
 import constants from "../../constants";
 
 const activeMembersCounter = async (guild: Guild, logChannel: GuildBasedChannel | undefined): Promise<void> => {
-	// const channelName: string = `Active Members: ${
-	// 	guild.members.cache.filter(
-	// 		(member) =>
-	// 			(member.presence?.status == "online" || member.presence?.status == "idle" || member.presence?.status == "dnd") && !member.user.bot
-	// 	).size
-	// }`;
-	// let channelId: Promise<any> | string;
-	// if (!guild.channels.cache.get("1031914728672473169")) {
-	// 	channelId = getOrCreateChannelId(guild, channelName, constants.VOICE_CHANNEL);
-	// } else {
-	// 	channelId = "1031914728672473169";
-	// }
-	const channelId = "1031914728672473169";
+	const channelName: string = `Active Members: ${
+		guild.members.cache.filter(
+			(member) =>
+				(member.presence?.status == "online" || member.presence?.status == "idle" || member.presence?.status == "dnd") && !member.user.bot
+		).size
+	}`;
+	const channelId: Promise<any> | string = getOrCreateChannelId(guild, channelName, constants.VOICE_CHANNEL);
 
 	setInterval(async () => {
-		const channel: GuildBasedChannel | undefined = guild.channels.cache.get(channelId);
+		const channel: GuildBasedChannel | undefined = guild.channels.cache.get(await channelId);
 		if (channel === undefined) throw new TypeError("The object must not be undefined nor null, aborting");
 		const onlineMembers = guild.members.cache.filter(
 			(member) =>
 				(member.presence?.status == "online" || member.presence?.status == "idle" || member.presence?.status == "dnd") && !member.user.bot
 		).size;
 		channel.setName(`Active Members: ${onlineMembers}`);
+		console.log(`Active Members updated: ${onlineMembers}`);
 	}, constants.ACTIVE_MEMBERS_REFRESH_INTERVAL);
 };
 export default activeMembersCounter;
